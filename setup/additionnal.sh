024fcf3c1b0f9e30479461aa027171be94a091f3
Axel Leroy <axeleroy@users.noreply.github.com>
2020-04-07 21:11:06 +0200

    Added ssh install and config (public key auth only)

--- setup/additionnal.sh
+++ setup/additionnal.sh
@@ -8,3 +8,6 @@ sudo apt install steam -y
 # Arrh
 sudo apt install transmission -y
 sudo apt install minidlna -y
+
+sudo apt install openssh-server -y
+echo "PasswordAuthentication no" | sudo tee -a /etc/ssh/sshd_config
