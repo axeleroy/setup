f4286c06ab84af961705ffd01f4110adf896e74a
Axel Leroy <axeleroy@users.noreply.github.com>
2020-04-10 13:28:58 +0200

    idea alias

--- setup/development.sh
+++ setup/development.sh
@@ -10,6 +10,9 @@ sudo apt install openjdk-8-jdk -y
 sudo apt install python3-pip -y
 sudo apt install webp -y
 
+# idea alias to quickly use IntelliJ as an advanced text editor
+echo "alias idea='intellij-idea-ultimate &> /dev/null'" >> ~/.bashrc
+
 git config --global user.email "axeleroy@users.noreply.github.com"
 git config --global user.name "Axel Leroy"
 git config --global core.editor "vi"
