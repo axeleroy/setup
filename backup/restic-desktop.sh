c7096598542a14ece2fe730024b301ee7e496db0
Axel Leroy <axeleroy@users.noreply.github.com>
2021-05-10 20:00:18 +0200

    Added a note for my future self

--- backup/restic-desktop.sh
+++ backup/restic-desktop.sh
@@ -3,6 +3,9 @@
 # Script to backup my desktop computer to my NAS using restic.
 # Password and repository are set in RESTIC_PASSWORD_FILE and
 # RESTIC_REPOSITORY respectively
+#
+# I'm using Restic's REST server, so my RESTIC_REPOSITORY looks
+# something like this: rest:http://host:8000/repo
 
 restic backup ~/ \
   --exclude-caches \
