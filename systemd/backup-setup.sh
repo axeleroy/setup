e1a2d5dcdc7d25e3ac57ba76fc7470cbc831d23c
Axel Leroy <axeleroy@users.noreply.github.com>
2025-02-04 18:25:34 +0100

    fix(systemd): backup should no longer run on startup

--- systemd/backup-setup.sh
+++ systemd/backup-setup.sh
@@ -6,7 +6,10 @@ then
   exit 1
 fi
 
-sudo cp backup-on-shutdown.service /etc/systemd/user/
-sudo sed -i "s#SHELL_SETUP_PATH#$SHELL_SETUP_PATH#g" /etc/systemd/user/backup-on-shutdown.service
+sudo cp backup-on-shutdown.service /etc/systemd/system/
+sudo sed -i "s#SHELL_SETUP_PATH#$SHELL_SETUP_PATH#g" /etc/systemd/system/backup-on-shutdown.service
+# setting user does not seem to help, should take a look into SELinux
+sudo sed -i "s#USER#$(id -u)#g" /etc/systemd/system/backup-on-shutdown.service
+sudo sed -i "s#GROUP#$(id -g)#g" /etc/systemd/system/backup-on-shutdown.service
 systemctl daemon-reload
-systemctl enable backup-on-shutdown.service --user
+systemctl enable backup-on-shutdown.service
