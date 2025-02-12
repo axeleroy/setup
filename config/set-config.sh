f48f0e9f0e11df4f7e57046922c037897c0d50cb
Axel Leroy <axeleroy@users.noreply.github.com>
2025-02-12 11:47:19 +0100

    chore(config): make sure files with underscore would be correctly linked

--- config/set-config.sh
+++ config/set-config.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 for file in $(pwd)/.*; do
-    [ -r "$file" ] && [ -f "$file" ] && [[ $file =~ (\.[a-z]+) ]] && ln -sb $file ~/${BATCH_REMATCH[1]};
+    [ -r "$file" ] && [ -f "$file" ] && [[ $file =~ (\.[a-z_]+) ]] && ln -sb $file ~/${BATCH_REMATCH[1]};
 done;
 unset file;
 
