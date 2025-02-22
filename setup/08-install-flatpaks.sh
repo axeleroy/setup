fe79f7b57257633e88643e2a0ebd8fe1a61855b4
Axel Leroy <axeleroy@users.noreply.github.com>
2025-02-22 23:58:12 +0100

    fix(setup): assume yes when installing Flatpaks

--- setup/08-install-flatpaks.sh
+++ setup/08-install-flatpaks.sh
@@ -6,10 +6,10 @@ source ${SHELL_SETUP_PATH}/setup/is-steamos.sh
 if [[ $(is_steamos) -eq 1 ]]; then exit 0; fi
 
 echo "Installing Zen"
-flatpak install app.zen_browser.zen
+flatpak install -y app.zen_browser.zen
 echo "Installing Valuta"
-flatpak install io.github.idevecore.Valuta
+flatpak install -y io.github.idevecore.Valuta
 echo "Installing Signal"
-flatpak install org.signal.Signal
+flatpak install -y org.signal.Signal
 echo "Installing Text Pieces"
-flatpak install io.gitlab.liferooter.TextPieces
+flatpak install -y io.gitlab.liferooter.TextPieces
