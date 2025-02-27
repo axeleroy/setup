d4a7d1afb95caab75892058c3d71ec376b3c25ba
Axel Leroy <axeleroy@users.noreply.github.com>
2025-02-27 11:00:42 +0100

    fix(setup): only apply git config to non-SteamOS

--- setup/09-set-up-gitconfig.sh
+++ setup/09-set-up-gitconfig.sh
@@ -2,5 +2,8 @@
 
 set -euo pipefail
 
+source ${SHELL_SETUP_PATH}/setup/is-steamos.sh
+if [[ $(is_steamos) -eq 1 ]]; then exit 0; fi
+
 ln -bs ${SHELL_SETUP_PATH}/config/.gitconfig ~/.gitconfig
 ln -bs ${SHELL_SETUP_PATH}/config/.gitignore ~/.gitignore
