a1fb11fbc69565df242eec52143f43cb93503af9
Axel Leroy <axeleroy@users.noreply.github.com>
2025-03-24 10:50:27 +0100

    fix(setup) specify flathub when installing flatpaks

--- setup/08-install-flatpaks.sh
+++ setup/08-install-flatpaks.sh
@@ -5,12 +5,12 @@ set -euo pipefail
 if [[ $(is_steamos) -eq 1 ]]; then exit 0; fi
 
 echo "Installing Zen"
-flatpak install -y app.zen_browser.zen
+flatpak install -y flathub app.zen_browser.zen
 echo "Installing Valuta"
-flatpak install -y io.github.idevecore.Valuta
+flatpak install -y flathub io.github.idevecore.Valuta
 echo "Installing Signal"
-flatpak install -y org.signal.Signal
+flatpak install -y flathub org.signal.Signal
 echo "Installing Text Pieces"
-flatpak install -y io.gitlab.liferooter.TextPieces
+flatpak install -y flathub io.gitlab.liferooter.TextPieces
 echo "Installing LocalSend"
-flatpak install -y org.localsend.localsend_app
+flatpak install -y flathub org.localsend.localsend_app
--- setup/11-install-gimp-inkscape.sh
+++ setup/11-install-gimp-inkscape.sh
@@ -5,6 +5,6 @@ set -euo pipefail
 if [[ $(is_steamos) -eq 1 ]]; then exit 0; fi
 
 echo "Installing GIMP"
-flatpak install -y org.gimp.GIMP
+flatpak install -y flathub org.gimp.GIMP
 echo "Installing Inkscape"
-flatpak install -y org.inkscape.Inkscape
+flatpak install -y flathub org.inkscape.Inkscape
