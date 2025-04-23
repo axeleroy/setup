b3a021cb911946c230206e6f2cd949721ff65930
Axel Leroy <axeleroy@users.noreply.github.com>
2025-04-23 13:56:16 +0200

    fix(setup): typo

--- setup/02-set-config.sh
+++ setup/02-set-config.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 echo "Linking dotfiles"
 for file in ${SHELL_SETUP_PATH}/config/.*; do
-    [ -r "$file" ] && [ -f "$file" ] && ln -sb $file ~/$$(basename ${file});
+    [ -r "$file" ] && [ -f "$file" ] && ln -sb $file ~/$(basename ${file});
 done;
 unset file;
 
