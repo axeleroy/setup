397f77a5ec1e30bdd8e514841d052cec55fc1ea0
Axel Leroy <axeleroy@users.noreply.github.com>
2025-04-26 23:35:56 +0200

    fix(setup): install Refine on Bazzite has GNOME Tweaks finally got removed

--- setup/16-install-removed-packages.sh
+++ setup/16-install-removed-packages.sh
@@ -7,7 +7,5 @@ if [[ $(is_hostname steamdeck) -eq 1 ]]; then exit 0; fi
 echo "Installing Document Scanner"
 flatpak install -y flathub org.gnome.SimpleScan
 
-# GNOME Tweaks is still on Bazzite, thus skip installing Refine
-if [[ $(is_hostname axel-Desktop) -eq 1 ]]; then exit 0; fi
 echo "Installing Refine"
 flatpak install -y flathub page.tesk.Refine
