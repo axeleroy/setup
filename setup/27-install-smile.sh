5f6cf9ab7ff3d16b7dcb0a8557177799bc68714d
Axel Leroy <axeleroy@users.noreply.github.com>
2025-11-04 16:45:53 +0100

    fix(setup): try setting keybinds for smile

--- setup/27-install-smile.sh
+++ setup/27-install-smile.sh
@@ -7,18 +7,21 @@ if [[ $(is_hostname steamdeck) -eq 1 ]]; then exit 0; fi
 echo "Installing Smile"
 flatpak install -y flathub it.mijorus.smile
 
+exit 0
+
 # Get latest custom keybinding ID
 KEYBINDS_JSON=$(gsettings get org.gnome.settings-daemon.plugins.media-keys custom-keybindings | tr "'" '"')
-KEYBIND_ID=$(echo $KEYBINDS_JSON | jq '.[-1:] | .[]' | grep -oP '(?<=custom)(\d)')
+KEYBIND_ID_NUMBER=$(echo $KEYBINDS_JSON | jq '.[-1:] | .[]' | grep -oP '(?<=custom)(\d)')
 # Increment ID
-KEYBIND_ID=$(($KEYBIND_ID + 1))
+KEYBIND_ID_NUMBER=$(($KEYBIND_ID_NUMBER + 1))
 
 echo "Creating new custom keybinding"
-KEYBINDS_JSON=$(echo $KEYBINDS_JSON | jq ". += [\"/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom$KEYBIND_ID/\"]" | tr '"' "'")
-gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings $KEYBINDS_JSON
-
-COMMAND="gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom$KEYBIND_ID/"
+KEYBIND_ID="/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom$KEYBIND_ID_NUMBER/"
+KEYBINDS_JSON=$(echo $KEYBINDS_JSON | jq ". += [\"$KEYBIND_ID\"]" | tr '"' "'" | tr -d "\n")
+COMMAND="gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:$KEYBIND_ID"
 
 $COMMAND name "Open Smile"
 $COMMAND command "flatpak run it.mijorus.smile"
-$COMMAND binding "<Ctrl><Alt>Space"
+$COMMAND binding "<Ctrl><Alt>space"
+
+gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "$KEYBINDS_JSON"
