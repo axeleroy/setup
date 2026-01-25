61ad091650a35a9464d0bef65122acf85225bc35
Axel Leroy <axeleroy@users.noreply.github.com>
2026-01-25 19:05:12 +0100

    fix(backup): moved excludes location

--- backup/docker-backup.sh
+++ backup/docker-backup.sh
@@ -2,4 +2,4 @@
 
 RESTIC_REPOSITORY=/mnt/restic-repos/restic \
 RESTIC_PASSWORD_FILE=/root/restic_pwd.txt \
-restic backup /var/lib/docker/volumes --exclude-file=/root/docker-excludes.txt
+restic backup /var/lib/docker/volumes --exclude-file=/home/axel/setup/backup/docker-excludes.txt
