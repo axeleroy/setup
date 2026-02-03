a7474184c11fcdd2bb4e7a723d8066181bfeaf21
Axel Leroy <axeleroy@users.noreply.github.com>
2026-02-03 15:30:43 +0100

    feat(systemd): service to update list of bots to block in Caddy
    Signed-off-by: Axel Leroy <axeleroy@users.noreply.github.com>

---
+++ utils/update-botslist.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+bots=$(curl -L https://github.com/ai-robots-txt/ai.robots.txt/raw/refs/heads/main/robots.json | jq -r 'keys | join("|")')
+bots="${bots}|NLUX_IAHarvester|AliyunSecBot|keys-so-bot"
+sed -i "s/@botForbidden header_regexp User-Agent \"[^\"]*\"/@botForbidden header_regexp User-Agent \"${bots}\"/" $CADDYFILE_PATH
